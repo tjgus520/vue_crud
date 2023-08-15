47d0bcaf149d867013f6aff08ed85d6e25916a94
tjgus520 <tjgus520@naver.com>
2023-08-15 22:08:16 +0900

    뷰 설치및 라우트 설치

---
+++ src/route.js
@@ -0,0 +1,15 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+const routes = [
+  {
+    path: "/경로",
+    component:
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router; 
